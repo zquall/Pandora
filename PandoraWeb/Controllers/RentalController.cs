7b32735a0e45d061788ac2999d7b7d44e25a7aa2
Jaime Torner <zquall@gmail.com>
2016-05-27 02:16:32 -0600

    Creacion de platilla de Documentos parte 1

---
+++ PandoraWeb/Controllers/RentalController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PandoraWeb.Controllers.Rental
+{
+    public class RentalController : Controller
+    {
+        // GET: RentalQuote
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Quote()
+        {
+            return View();
+        }
+
+        // GET: RentalQuote/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RentalQuote/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RentalQuote/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RentalQuote/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RentalQuote/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RentalQuote/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RentalQuote/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PandoraWeb/Models/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PandoraWeb.Models
+{
+    public class Class1
+    {
+    }
+}
