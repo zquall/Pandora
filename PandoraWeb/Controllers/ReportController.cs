2671a640a89180632da4671e89b6a59b2dd531e4
Kaji <cavalosm24@gmail.com>
2016-06-07 19:13:06 -0600

    PivotGrid y ReportController

---
+++ PandoraWeb/Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using Core;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PandoraWeb.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SalesByEmployeeByDivision()
+        {
+            return View();
+        }
+
+        [ValidateInput(false)]
+        public ActionResult SalesPivotGridPartial()
+        {
+            var service = new ReportSalesService();
+            var model = service.GetSalesByEmployeeByDivision(DateTime.Today.AddMonths(-3), DateTime.Today);
+            return PartialView("_SalesPivotGridPartial", model);
+        }
+    }
+
+}
