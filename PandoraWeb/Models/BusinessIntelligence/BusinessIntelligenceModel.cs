9480781153a39974b046d1390a12a42a2fadb126
Jaime Torner <zquall@gmail.com>
2016-06-09 21:02:34 -0600

    mising files

---
+++ PandoraWeb/Models/BusinessIntelligence/BusinessIntelligenceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DevExpress.Utils;
+using DevExpress.Web.Mvc;
+using DevExpress.XtraPivotGrid;
+using PandoraWeb.Models.PivotGrid;
+
+namespace PandoraWeb.Models.BusinessIntelligence
+{
+    public class BusinessIntelligenceModel
+    {
+        public PivotGridSettings PivotGridSettings { get; set; }
+        public IEnumerable<object> BindData { get; set; }
+    }
+}
