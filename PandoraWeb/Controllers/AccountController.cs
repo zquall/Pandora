d8926b66c6557173212013596c6b0cbb4517d8df
Kaji <cavalosm24@gmail.com>
2016-06-27 20:25:14 -0600

    Log In

---
+++ PandoraWeb/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PandoraWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
